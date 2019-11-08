a7c2afd00774f6ba7f3381ebe17a9a5aa9fca86d
Ilya Chesalin <evilyach@protonmail.com>
2019-11-08 21:07:13 +0300

    Added basic web server

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+import cfg
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run(cfg.hostname, cfg.port, debug=True)
