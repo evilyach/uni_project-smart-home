81c373dd7caaa0e3ea530ac20abceba18abb31ca
Ilya Chesalin <evilyach@protonmail.com>
2019-12-20 00:40:50 +0000

    [#4] Added most of functionality of addDevice

--- frontend/src/layouts/layout.vue
+++ frontend/src/layouts/layout.vue
@@ -51,7 +51,7 @@
 
         <q-item clickable to="/rawAreas">
           <q-item-section avatar>
-            <q-icon name="view_list" />
+            <q-icon name="map" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Пространства</q-item-label>
@@ -59,19 +59,39 @@
           </q-item-section>
         </q-item>
 
-        <q-item clickable to="/rawDeviceTypes">
+        <q-item clickable to="/rawCommands">
           <q-item-section avatar>
-            <q-icon name="view_list" />
+            <q-icon name="contactless" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Типы устройств</q-item-label>
-            <q-item-label caption>Таблица "Типы устройств"</q-item-label>
+            <q-item-label>Команды</q-item-label>
+            <q-item-label caption>Таблица "Команды"</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/rawCommandsByDevice">
+          <q-item-section avatar>
+            <q-icon name="device_hub" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Управление</q-item-label>
+            <q-item-label caption>Таблица "Управление"</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/rawCommandTypes">
+          <q-item-section avatar>
+            <q-icon name="contactless" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Типы команд</q-item-label>
+            <q-item-label caption>Таблица "Типы команд"</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-item clickable to="/rawDevices">
           <q-item-section avatar>
-            <q-icon name="view_list" />
+            <q-icon name="devices_other" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Устройства</q-item-label>
@@ -79,9 +99,49 @@
           </q-item-section>
         </q-item>
 
+        <q-item clickable to="/rawDeviceParameters">
+          <q-item-section avatar>
+            <q-icon name="perm_device_information" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Типы устройств</q-item-label>
+            <q-item-label caption>Таблица "Типы устройств"</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/rawDeviceTypes">
+          <q-item-section avatar>
+            <q-icon name="devices_other" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Типы устройств</q-item-label>
+            <q-item-label caption>Таблица "Типы устройств"</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/rawDevicesByUserAccount">
+          <q-item-section avatar>
+            <q-icon name="device_hub" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Контролируемые учетной записью устройства</q-item-label>
+            <q-item-label caption>Таблица "Контролируемые учетной записью устройства"</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/rawDevicesInProfile">
+          <q-item-section avatar>
+            <q-icon name="device_hub" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Содержащиеся в профиле устройства</q-item-label>
+            <q-item-label caption>Таблица "Содержащиеся в профиле устройства"</q-item-label>
+          </q-item-section>
+        </q-item>
+
         <q-item clickable to="/rawProfiles">
           <q-item-section avatar>
-            <q-icon name="view_list" />
+            <q-icon name="person_outline" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Профили</q-item-label>
@@ -89,19 +149,9 @@
           </q-item-section>
         </q-item>
 
-        <q-item clickable to="/rawRealEstateTypes">
-          <q-item-section avatar>
-            <q-icon name="view_list" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Типы недвижимости</q-item-label>
-            <q-item-label caption>Таблица "Типы недвижимости"</q-item-label>
-          </q-item-section>
-        </q-item>
-
         <q-item clickable to="/rawRealEstates">
           <q-item-section avatar>
-            <q-icon name="view_list" />
+            <q-icon name="king_bed" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Недвижимость</q-item-label>
@@ -109,9 +159,19 @@
           </q-item-section>
         </q-item>
 
+        <q-item clickable to="/rawRealEstateTypes">
+          <q-item-section avatar>
+            <q-icon name="location_city" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Типы недвижимости</q-item-label>
+            <q-item-label caption>Таблица "Типы недвижимости"</q-item-label>
+          </q-item-section>
+        </q-item>
+
         <q-item clickable to="/rawUserAccounts">
           <q-item-section avatar>
-            <q-icon name="view_list" />
+            <q-icon name="people" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Пользователи</q-item-label>
@@ -119,9 +179,39 @@
           </q-item-section>
         </q-item>
 
+        <q-item clickable to="/rawUserAccountTypes">
+          <q-item-section avatar>
+            <q-icon name="group" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Типы пользователей</q-item-label>
+            <q-item-label caption>Таблица "Типы пользователей"</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/rawUserAccountRights">
+          <q-item-section avatar>
+            <q-icon name="border_clear" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Права пользователей</q-item-label>
+            <q-item-label caption>Таблица "Права пользователей"</q-item-label>
+          </q-item-section>
+        </q-item>
+
         <!---->
 
         <q-item-label header>Настройки</q-item-label>
+
+        <q-item clickable to="/log">
+          <q-item-section avatar>
+            <q-icon name="history" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Журнал</q-item-label>
+            <q-item-label caption>Журнал событий информационной системы</q-item-label>
+          </q-item-section>
+        </q-item>
 
         <q-item clickable to="/about">
           <q-item-section avatar>
--- frontend/src/pages/addDevice.vue
+++ frontend/src/pages/addDevice.vue
@@ -1,16 +1,344 @@
 <template>
-    <q-page class="flex flex-center">
-      <device-type-button />
-    </q-page>
+  <q-page class="flex flex-center">
+    <q-splitter
+      v-model="splitterModel"
+      style="width: 90%"
+    >
+      <template v-slot:before>
+        <q-tabs
+          v-model="tab"
+          vertical
+        >
+          <q-tab name="fridge" icon="kitchen" label="Умный холодильник" />
+          <q-tab name="washing-machine" icon="fas fa-tshirt" label="Умная стиральная машина" />
+          <q-tab name="robot-vacuum" icon="fas fa-robot" label="Робот-пылесос" />
+          <q-tab name="video-surveillance" icon="camera" label="Система видеонаблюдения" />
+          <q-tab name="kettle" icon="fas fa-mug-hot" label="Интеллектуальный чайник" />
+          <q-tab name="coffee-maker" icon="fas fa-coffee" label="Кофеварка" />
+          <q-tab name="scales" icon="fas fa-weight" label="Умные кухонные весы" />
+          <q-tab name="dish-washer" icon="room_service" label="Посудомоечная машина" />
+          <q-tab name="lock" icon="lock" label="Умный замок" />
+          <q-tab name="ac" icon="ac_unit" label="Климатконтроль" />
+          <q-tab name="lamp" icon="fas fa-lightbulb" label="Умная лампочка" />
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="tab"
+          animated
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="fridge" style="width: 100%">
+            <div class="text-h4 q-mb-md">Умный холодильник</div>
+
+            <q-input
+              v-model="text_fridge_ip"
+              filled
+              label="IP-адрес"
+              hint="Введите сетевой адрес устройства"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_fridge_password"
+              filled
+              label="Пароль"
+              hint="Введите пароль доступа"
+              style="width: 99%"
+              :type="fridge_isPwd ? 'password' : 'text'"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="fridge_isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="fridge_isPwd = !fridge_isPwd"
+                />
+              </template>
+            </q-input>
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_fridge_temperature"
+              filled
+              label="Температура"
+              hint="Введите температуру работы холодильника"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-btn name="fridge_add" outline rounded color="primary" label="Добавить" />
+          </q-tab-panel>
+
+          <!-- ------- -->
+
+          <q-tab-panel name="washing-machine" style="width: 100%">
+            <div class="text-h4 q-mb-md">Умная стиральная машина</div>
+
+            <q-input
+              v-model="text_washing_machine_ip"
+              filled
+              label="IP-адрес"
+              hint="Введите сетевой адрес устройства"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_washing_machine_password"
+              filled
+              label="Пароль"
+              hint="Введите пароль доступа"
+              style="width: 99%"
+              :type="washing_machine_isPwd ? 'password' : 'text'"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="washing_machine_isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="washing_machine_isPwd = !washing_machine_isPwd"
+                />
+              </template>
+            </q-input>
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_washing_machine_speed"
+              filled
+              label="Скорость"
+              hint="Введите скорость вращения барабана"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-btn outline rounded color="primary" label="Добавить" />
+          </q-tab-panel>
+
+          <!-- ------- -->
+
+          <q-tab-panel name="robot-vacuum" style="width: 100%">
+            <div class="text-h4 q-mb-md">Робот-пылесос</div>
+
+            <q-input
+              v-model="text_robot_vacuum_ip"
+              filled
+              label="IP-адрес"
+              hint="Введите сетевой адрес устройства"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_robot_vacuum_password"
+              filled
+              label="Пароль"
+              hint="Введите пароль доступа"
+              style="width: 99%"
+              :type="robot_vacuum_isPwd ? 'password' : 'text'"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="robot_vacuum_isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="robot_vacuum_isPwd = !robot_vacuum_isPwd"
+                />
+              </template>
+            </q-input>
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_robot_vacuum_speed"
+              filled
+              label="Скорость"
+              hint="Введите скорость передвижения пылесоса"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_robot_vacuum_max_value"
+              filled
+              label="Количество комнат"
+              hint="Введите количество комнат, в которых пылесос будет вести работу"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-btn outline rounded color="primary" label="Добавить" />
+          </q-tab-panel>
+
+          <!-- ------- -->
+
+          <q-tab-panel name="video-surveillance" style="width: 100%">
+            <div class="text-h4 q-mb-md">Система видеонаблюдения</div>
+
+            <q-input
+              v-model="text_video_surveillance_ip"
+              filled
+              label="IP-адрес"
+              hint="Введите сетевой адрес устройства"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_video_surveillance_password"
+              filled
+              label="Пароль"
+              hint="Введите пароль доступа"
+              style="width: 99%"
+              :type="video_surveillance_isPwd ? 'password' : 'text'"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="video_surveillance_isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="video_surveillance_isPwd = !video_surveillance_isPwd"
+                />
+              </template>
+            </q-input>
+
+            <div style="height: 20px" />
+
+            <q-checkbox
+              right-label
+              v-model="video_surveillance_to_alarm"
+              label="Включить оповещение?"
+            />
+
+            <div style="height: 20px" />
+
+            <q-btn outline rounded color="primary" label="Добавить" />
+          </q-tab-panel>
+
+          <!-- ------- -->
+
+          <q-tab-panel name="kettle" style="width: 100%">
+            <div class="text-h4 q-mb-md">Интеллектуальный чайник</div>
+
+            <q-input
+              v-model="text_kettle_ip"
+              filled
+              label="IP-адрес"
+              hint="Введите сетевой адрес устройства"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_kettle_password"
+              filled
+              label="Пароль"
+              hint="Введите пароль доступа"
+              style="width: 99%"
+              :type="kettle_isPwd ? 'password' : 'text'"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="kettle_isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="kettle_isPwd = !kettle_isPwd"
+                />
+              </template>
+            </q-input>
+
+            <div style="height: 20px" />
+
+            <q-color v-model="kettle_color" class="my-picker" />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_kettle_temperature"
+              filled
+              label="Время работы"
+              hint="Введите температуру работы чайника"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-input
+              v-model="text_kettle_speed"
+              filled
+              label="Скорость"
+              hint="Введите скорость закипания чайника (в секундах)"
+              style="width: 99%"
+            />
+
+            <div style="height: 20px" />
+
+            <q-checkbox
+              right-label
+              v-model="kettle_to_alarm"
+              label="Включить оповещение?"
+            />
+
+            <div style="height: 20px" />
+
+            <q-btn outline rounded color="primary" label="Добавить" />
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+    </q-splitter>
+  </q-page>
 </template>
 
 <script>
-import DeviceTypeButton from '../components/Buttons/DeviceTypeButton'
-
 export default {
-  name: "PageIndex",
-  components: [
-    DeviceTypeButton
-  ]
+  name: "AddDevice",
+  data () {
+    return {
+      tab: 'fridge',
+
+      text_fridge_ip: '192.168.0.1',
+      text_fridge_password: '',
+      text_fridge_temperature: '',
+      fridge_isPwd: true,
+
+      text_washing_machine_ip: '192.168.0.1',
+      text_washing_machine_password: '',
+      text_washing_machine_speed: '',
+      washing_machine_isPwd: true,
+
+      text_robot_vacuum_ip: '192.168.0.1',
+      text_robot_vacuum_password: '',
+      text_robot_vacuum_speed: '',
+      text_robot_vacuum_max_value: '',
+      robot_vacuum_isPwd: true,
+
+      text_video_surveillance_ip: '192.168.0.1',
+      text_video_surveillance_password: '',
+      video_surveillance_to_alarm: true,
+      video_surveillance_isPwd: true,
+
+      text_kettle_ip: '192.168.0.1',
+      text_kettle_password: '',
+      text_kettle_temperature: '',
+      text_kettle_speed: '',
+      kettle_color: '#FF00FF',
+      kettle_to_alarm: true,
+      kettle_temperature: '',
+    }
+  },
 };
 </script>
+
+<style lang="sass" scoped>
+.my-picker
+  max-width: 250px
+</style>
