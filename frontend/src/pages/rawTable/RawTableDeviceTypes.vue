b4a80d1ec55ada72b0cf5707825ae1f3a4574db6
Ilya Chesalin <evilyach@protonmail.com>
2019-12-22 16:40:02 +0000

    [#26] Connected Device Types table to a DB

--- frontend/src/pages/rawTable/RawTableDeviceTypes.vue
+++ frontend/src/pages/rawTable/RawTableDeviceTypes.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="q-pa-sm">
-    <q-table title="Типы устройств" :data="data" :columns="columns" row-key="name">
+    <q-table
+      title="Типы устройств"
+      :data="data"
+      :columns="columns"
+    >
       <template v-slot:body-cell-name="props">
         <q-td :props="props">
           <div>
@@ -16,80 +20,74 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       columns: [
         {
-          name: "name",
-          required: true,
-          label: "Название столбца",
+          name: "id",
           align: "center",
+          label: "ID устройства",
+          field: row => row.id,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: "name",
+          align: "center",
+          label: "Имя",
           field: row => row.name,
           format: val => `${val}`,
           sortable: true
         },
         {
-          name: "device_type_id",
+          name: "description",
           align: "center",
-          label: "Идентификатор типа устройства",
-          field: row => row.device_type_id,
-          format: val => `${val}`,
-          sortable: true
-        },
-        {
-          name: "device_type_type",
-          align: "center",
-          label: "Тип устройства",
-          field: row => row.device_type_type,
-          format: val => `${val}`,
-          sortable: true
-        },
-        {
-          name: "device_type_description",
-          align: "justify",
           label: "Описание",
-          field: row => row.device_type_description,
+          field: row => row.description,
           format: val => `${val}`,
           sortable: true
-        }
+        },
       ],
-      data: [
-        {
-          name: "Кондиционер",
-          device_type_id: "1",
-          device_type_type: "Кондиционер",
-          device_type_description: "Кондиционе́р (англ. conditioner) — устройство для поддержания оптимальных климатических условий в помещениях строительных сооружений, транспортных средств и другой техники.",
-        },
-        {
-          name: "Умный вентилятор",
-          device_type_id: "2",
-          device_type_type: "Умный вентилятор",
-          device_type_description: "Вентилятор — устройство для перемещения газа со степенью сжатия менее 1,15 (или разностью давлений на выходе и входе не более 15 кПа, при большей разнице давлений используют компрессор).",
-        },
-        {
-          name: "Умный чайник",
-          device_type_id: "3",
-          device_type_type: "Умный чайник",
-          device_type_description: "Ча́йник — небольшой закрытый сосуд с носиком, крышкой (как правило, хотя бывают и исключения — см. фото) и ручкой для подогревания и кипячения воды. Обычно чайники изготавливаются из металла.",
-        },
-        {
-          name: "LED лампа",
-          device_type_id: "4",
-          device_type_type: "LED лампа",
-          device_type_description: "Светодиодное освещение — одно из перспективных направлений технологий искусственного освещения, основанное на использовании светодиодов в качестве источника света.",
-        }
-      ]
-    };
-  }
-};
+      data: [],
+      getData() {
+        axios.get("http://localhost:13491/api/raw/device_type")
+          .then(res => {
+            const data = res['data'][0];
+
+            data.forEach(element => {
+              const id = element[0][0];
+              const name = element[0][1];
+              const description = element[0][2];
+
+              this.data.push({
+                "id": id,
+                "name": name,
+                "description": description
+              });
+            });
+          })
+          .catch(e => {
+            this.$q.notify({
+              message: "Не удалось получить доступ к базе данных: " + e,
+              color: "negative"
+            });
+          });
+      },
+    }
+  },
+  created() {
+    this.getData();
+  },
+}
 </script>
 
 <style>
 .my-table-details {
   font-size: 0.85em;
   font-style: italic;
-  max-width: 300px;
   white-space: normal;
   color: #555;
   margin-top: 4px;
